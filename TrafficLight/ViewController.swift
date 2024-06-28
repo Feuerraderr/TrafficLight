43a011c5397bce1c46b49ed1f38c9d2415daf24d
Feuerraderr <belov-vlad2008@yandex.ru>
2024-06-29 01:39:28 +0300

    Logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,11 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    private enum TrafficLight {
+        case start
+        case red
+        case yellow
+        case green
+    }
+    
+    private var lightColor: TrafficLight = .start
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        redLightView.alpha = 0.1
+        
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        yellowLightView.alpha = 0.1
+        
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+        greenLightView.alpha = 0.1
+        
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startButtonDidTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch lightColor {
+        case .start:
+            redLightView.alpha = 1
+            lightColor = .red
+        case .red:
+            redLightView.alpha = 0.1
+            yellowLightView.alpha = 1
+            lightColor = .yellow
+        case .yellow:
+            yellowLightView.alpha = 0.1
+            greenLightView.alpha = 1
+            lightColor = .green
+        case .green:
+            greenLightView.alpha = 0.1
+            redLightView.alpha = 1
+            lightColor = .red
+        }
+    }
+    
 }
 
